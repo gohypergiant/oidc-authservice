a4452dc21dd5e55cff1b7ad1a13a60c38da2c552
Brandon Martel <brandonmartel@gmail.com>
2021-05-31 12:21:07 -0500

    fixing arg typos

--- roles.go
+++ roles.go
@@ -11,9 +11,9 @@ type Roles = []string
 
 func getRolesByEmail(svcUrl string, email string) *Roles {
 	svcByteString := []byte(svcUrl)
-	url := fmt.Sprintf("%s/%s", bytes.TrimRight("/"), email)
+	url := fmt.Sprintf("%s/%s", bytes.TrimRight(svcByteString, "/"), email)
 
-	req, err := http.NewRequest("GET", url)
+	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", "<idToken>"))
 	req.Header.Set("Content-Type", "application/json")
 
