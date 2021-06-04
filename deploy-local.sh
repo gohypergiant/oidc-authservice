55f9a108c77e8bf6372514145468cc2638cba123
kalai-muthuswamy <79482702+kalai-muthuswamy@users.noreply.github.com>
2021-06-04 13:55:55 -0500

    Update deploy-local.sh

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t hyperdrive-oidc-authservice .
+docker build -t hyperdrive-oidc-authservice -t hyperdrive-oidc-authservice:latest -f docker/oidc-authservice-dev.Dockerfile .
 k3d image import --cluster hyperdrive-local hyperdrive-oidc-authservice:latest
 kubectl rollout restart statefulset -n istio-system authservice
